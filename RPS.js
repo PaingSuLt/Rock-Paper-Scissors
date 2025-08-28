e93731b009723125224265c5e55c12e05d4b8d86
PaingSuLt <pkhazel72@gmail.com>
2025-08-29 04:13:05 +0630

    :wq

--- RPS.js
+++ RPS.js
@@ -1,23 +1,63 @@
-
+let computerChoice;
+let humanChoice;
 
 function getComputerChoice() {
-    let choice = Math.random() * 3;
-    
-    
+
+    computerChoice = Math.floor(Math.random()*3);
+
+    if(computerChoice===0) {
+        return "ROCK";
+    }else if(computerChoice===1) {
+        return "PAPER";
+    }else {
+        return "SCISSORS";
+    }
+}
+
+function getHumanChoice() {
+
+    humanChoice = prompt("Rock, Paper or Scissors? Type one and Press Enter");
+
+    humanChoice = humanChoice.toUpperCase();
+    return humanChoice;
 
 }
 
-getComputerChoice;
+let computerScore = 0;
+let humanScore = 0;
 
-let text;
-let choice = Math.floor(Math.random() * 3);
-if (choice==0) {
-    text = `Rock`;
+function playRound(humanChoice, computerChoice) {
+
+    if(computerChoice==="ROCK" && humanChoice==="PAPER") {
+        alert(`You:${humanChoice}
+            Computer:${computerChoice}`)
+            humanScore += 1;
+    }else if(computerChoice==="PAPER" && humanChoice==="SCISSORS") {
+        alert(`You:${humanChoice}
+            Computer:${computerChoice}`)
+            humanScore += 1;
+    }else if(computerChoice==="SCISSORS" && humanChoice==="ROCK") {
+        alert(`You:${humanChoice}
+            Computer:${computerChoice}`)
+            humanScore += 1;
+    }else if(computerChoice==="SCISSORS" && humanChoice==="PAPER") {
+        alert(`You:${humanChoice}
+            Computer:${computerChoice}`)
+            computerScore += 1;
+    }else if(computerChoice==="PAPER" && humanChoice==="ROCK") {
+        alert(`You:${humanChoice}
+            Computer:${computerChoice}`)
+            computerScore += 1;
+    }else if(computerChoice==="ROCK" && humanChoice==="SCISSORS") {
+        alert(`You:${humanChoice}
+            Computer:${computerChoice}`)
+            computerScore += 1;
+    }else {
+        alert(`DRAW!`)
+    }
 }
-if (choice==1) {
-    text = `Paper`;
-}
-if (choice==2) {
-    text = `Scissor`;
-}
-console.log(`Computer: ${text}`);
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
